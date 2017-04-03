a5c837651675f517a1bd8d6dcc7aa28237d92d0e
Loay Abdelrazek <SigPloiter@users.noreply.github.com>
2017-04-03 11:22:14 -0400

    Add files via upload

---
+++ SS7/installer.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+
+#Download SS7 Stack from JSS7
+download()
+{
+	url=$1
+	wget --progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}' 
+	echo -ne "\b\b\b\b"
+	
+}
+
+FILE="restcomm-jss7-7.2.1390.zip"
+REST_DIR="restcomm-jss7-7.2.1390"
+
+#Cleaning if any previous download occurred that may be corrupted or interrupted
+if [ -f "$FILE" ]
+then
+        echo -e '\033[34m[*]\033[0mDetected SS7 Stack File..Removing it'
+        rm -f $FILE
+        echo -e '\033[34m[*]\033[0mDownloading a fresh version of '"$FILE"'..'
+fi
+
+
+
+if [ -d "$REST_DIR" ]
+then
+	echo -e '\033[34m[*]\033[0mDetected SS7 Stack Directory..Removing it'
+	rm -rf $REST_DIR
+	
+fi
+
+#Downloading the Stack
+echo -e '\033[34m[*]\033[0mRetrieving SS7 Stack'
+download "https://github.com/RestComm/jss7/releases/download/7.2.1390/$FILE"
+
+if [ $? == 0 ]
+then
+	echo -e '\n\033[32m[+]\033[0mSS7 Stack Downloaded..'
+else
+	echo -e '\033[31m[-]\033[0mError Downloading the Stack..'
+	exit 1
+fi
+
+#Unzipping
+unzip $FILE
+rm -f $FILE
+
+
+#Setting CLASSPATH Variable
+export SIGPLOIT=$PWD/restcomm-jss7-7.2.1390/ss7/restcomm-ss7-service/lib/*
+echo 'export SIGPLOIT=$PWD/restcomm-jss7-7.2.1390/ss7/restcomm-ss7-service/lib/*' >> ~/.bashrc
+echo 'export SIGPLOIT=$PWD/restcomm-jss7-7.2.1390/ss7/restcomm-ss7-service/lib/*' >> ~/.bash_profile
+
+source ~/.bashrc
+source ~/.bash_profile
+
+
+
+
+if [ "$SIGPLOIT" == "$PWD/restcomm-jss7-7.2.1390/ss7/restcomm-ss7-service/lib/*" ]
+then
+	echo -e '\n\033[32m[+]\033[0mSiGploit is Ready for use..run SiGploit.py'
+	exit 0
+else
+	echo -e '\033[31m[-]\033[0mError Setting CLASSPATH..'
+	exit 1
+fi
